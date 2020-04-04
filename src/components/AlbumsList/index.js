9ccddf700e14f2b2842d1d26b92071adae29e1c5
Ranzel Manalo <rmanalo@getdevs.com>
2020-04-04 22:15:10 +0800

    new design

--- src/components/AlbumsList/index.js
+++ src/components/AlbumsList/index.js
@@ -1,33 +1,34 @@
-import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import React, { Component, useState } from "react";
+import { ScrollView, FlatList } from "react-native";
 
-import AlbumItem from '../AlbumItem'
-import styles from './styles'
+import AlbumItem from "../AlbumItem";
+import styles from "./styles";
 
-class AlbumsList extends Component {
-  constructor( props ) {
-    super( props );
-    this.state = {};
-  }
+const AlbumsList = props => {
+  const { albums } = props;
 
-  render() {
-    let { albums } = this.props;
-
+  const renderThisItems = (item, index) => {
     return (
-      <ScrollView style={{flex:1, backgroundColor: 'white'}}>
-        {albums.map((album, index) => (
-          <AlbumItem
-            key={index}
-            albumName={album.albumName}
-            thumbnail={album.images[0].image.uri}
-            counter={album.images.length || 0}
-            index={index}
-            onAlbumPress={this.props.onAlbumPress} />
-        ))}
-      </ScrollView>
+      <AlbumItem
+        key={index}
+        albumName={item.albumName}
+        thumbnail={item.images[0].image.uri}
+        counter={item.images.length || 0}
+        index={index}
+        onAlbumPress={props.onAlbumPress}
+      />
     );
-  }
+  };
 
-}
+  return (
+    <FlatList
+      style={{ flex: 1, backgroundColor: "white" }}
+      data={albums}
+      renderItem={({ item, index }) => renderThisItems(item, index)}
+      keyExtractor={({ item, index }) => index}
+      numColumns={2}
+    />
+  );
+};
 
 export default AlbumsList;
